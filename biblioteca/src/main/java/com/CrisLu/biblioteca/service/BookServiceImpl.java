03d75446f6bcaafefb8e9fb885a3544137c6d55a
Alvarado Rodríguez <lucy-elena.alvarado-rodriguez@capgemini.com>
2023-01-11 09:04:44 +0100

    Book Service implement

---
+++ biblioteca/src/main/java/com/CrisLu/biblioteca/service/BookServiceImpl.java
@@ -0,0 +1,60 @@
+package com.CrisLu.biblioteca.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.CrisLu.biblioteca.model.Book;
+import com.CrisLu.biblioteca.repository.BookRepository;
+
+@Service
+public class BookServiceImpl implements BookService{
+
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Override
+	public List<Book> getAllBook() {
+		
+		return this.bookRepository.findAll();
+	}
+
+	@Override
+	public Book getBookById(long id) {
+		Optional<Book> optionalBook = this.bookRepository.findById(id);
+		Book book = null;
+		if(optionalBook.isPresent()) {
+			book = optionalBook.get();
+		}else {
+			throw new RuntimeException("el libro con id: " + id + "no se encuentra");
+		}
+		return book;
+	}
+
+	@Override
+	public void saveBook(Book book) {
+		this.bookRepository.save(book);
+		
+	}
+
+	@Override
+	public void deleteBookById(long id) {
+		this.bookRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public Page<Book> findPaginate(int pageNum, int pageSize, String sortField, String sortDirection) {
+		Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name())?
+				Sort.by(sortField).ascending(): Sort.by(sortField).descending();
+		Pageable pageable = PageRequest.of(pageNum -1, pageSize, sort);
+		return this.bookRepository.findAll(pageable);
+	}
+
+}
