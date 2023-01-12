e2f3d2370ef5eb6f75712e6eddc85ab1b7005485
Cristina Sanchez <c.sanchez.cadrecha@gmail.com>
2023-01-12 14:03:32 +0100

    added Front styles

--- biblioteca/src/main/java/com/CrisLu/biblioteca/controller/HomeController.java
+++ biblioteca/src/main/java/com/CrisLu/biblioteca/controller/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
+	
+	@GetMapping("/pageHome")
+	public String viewHomePage() {
+		return "redirect:/";
+	}
 	@GetMapping("/pageBook")
 	public String viewBookPage() {
 		return "redirect:/book";
